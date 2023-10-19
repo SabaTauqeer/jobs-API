314c8ab9385feb943d1b0b2f40b5c581a48812b1
SabaTauqeer <sabatauqeer1@gmail.com>
2023-10-19 16:22:40 +0500

    ss

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     console.log(authHeader);
-    //   throw new UnauthenticatedError("please provide credentials");
+    throw new UnauthenticatedError("please provide credentials");
   }
   const token = authHeader.split(" ")[1];
   try {
