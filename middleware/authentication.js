aecc5b7a16b258f23fa21621581a7dcc88e87d8f
SabaTauqeer <sabatauqeer1@gmail.com>
2023-10-19 16:04:15 +0500

    s

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const { UnauthenticatedError } = require("../errors");
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new UnauthenticatedError("please provide credentials");
   }
